2673130d400c6523d088491100ea8cd041a24c28
Squater0219 <alswnddl02@gmail.com>
2024-06-21 21:37:37 +0900

    textfilewriter

--- libs/textfilewriter.c
+++ libs/textfilewriter.c
@@ -7,13 +7,32 @@
 #include "textfilewriter.h"
 
 void create_music_titles(FILE* stream){
-    printf("Test\n");
+    fprintf(stream, "3\nHello\nEnemy\nabc\n");
 }
 
 void read_file(char* file_name){
+    printf("Loading playlist from file: %s\n", file_name);
+    load(file_name);
 
+    printf("Loaded Playlist:\n");
+    list();
+
+    clear();
 }
 
 void write_file(char* file_name){
+    add("Hello");
+    add("Enemy");
+    add("abc");
 
+    FILE* file = fopen(file_name, "w");
+    if (!file) {
+        printf("Error opening file %s.\n", file_name);
+        return;
+    }
+
+    printf("Saving playlist to file: %s\n", file_name);
+    print_file(file);
+    fclose(file);
+    clear();
 }
